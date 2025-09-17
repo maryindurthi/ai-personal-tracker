069ec341fa0887cddb186898191198b67457d02f
Indurthi <maryindurthi301@gmail.com>
2025-09-17 18:56:52 -0500

    Add DailyTracker page

---
+++ saas/ai-tracker/src/app/daily-tracker/page.js
@@ -0,0 +1,11 @@
+"use client";
+
+import DailyTracker from "../../components/DailyTracker";
+
+export default function DailyTrackerPage() {
+  return (
+    <div className="min-h-screen p-8">
+      <DailyTracker />
+    </div>
+  );
+}
